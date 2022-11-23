a2247abcc0932b1f96823cbd2cb7f03838f8dba9
quisethecoder <marquise_dawkins@aol.com>
2022-11-22 21:22:18 -0500

    clock edit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Changing the day in HTML below
+
+// function today(){
+// let month = new Date().getMonth;
+// let day = new Date().getDay;
+// let year = new Date().getFullYear;
+// let currentDay = month + "/" + day + "/" + year;
+
+// document.querySelector(".date").innerHTML = currentDay;
+// }
+
+// today();
+ 
+
+// changing the time in HTML below
+
+// let hour = new Date().getHours;
+// let minute = new Date().getMinutes;
+// let seconds = new Date().getSeconds;
+
+// let clock = hour + ":" + minute + ":" + seconds;
+
+// document.getElementById("clock").innerHTML = clock
