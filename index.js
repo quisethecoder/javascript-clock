5e92c286c66a7c41ee4c0b7eedb6e999cda33c1e
quisethecoder <marquise_dawkins@aol.com>
2022-11-24 00:19:46 -0500

    change date

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-// Changing the day in HTML below
-
-// function today(){
-    
-// let month = new Date().getMonth;
-// let day = new Date().getDay;
-// let year = new Date().getFullYear;
-// let currentDay = month + "/" + day + "/" + year;
-
-// document.querySelector(".date").innerHTML = currentDay;
-// }
-
-// today();
- 
-
-// changing the time in HTML below
-
-// let hour = new Date().getHours;
-// let minute = new Date().getMinutes;
-// let seconds = new Date().getSeconds;
-
-// let clock = hour + ":" + minute + ":" + seconds;
-
-// document.getElementById("clock").innerHTML = clock
-
-
 let currentDate = new Date();
 
-const day = document.querySelector(".date");
+const date = document.querySelector(".date");
 const time = document.querySelector(".clock");
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function getDay(){
+    let currentDate = new Date();
+    let month = currentDate.getMonth()+1;
+    let day = currentDate.getDate();
+    let year = currentDate.getFullYear();
 
+    date.innerHTML = month + "/" + day + "/" + year;
+}
+
+getDay();
+
+function getTime(){
+    let 
+}
