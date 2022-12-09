58e486fda7bfd3b33ab7d997705005bd67881363
quisethecoder <marquise_dawkins@aol.com>
2022-12-08 19:50:14 -0500

    final version

--- index.js
+++ index.js
@@ -15,78 +15,36 @@ function getDay(){
 }
 
 getDay();
- 
-setInterval(getTime,1000);
 
-function getTime(){
-    let currentDate = new Date();
-    let hour = currentDate.getHours();
-    let minute = currentDate.getMinutes();
-    let seconds = currentDate.getSeconds();
+// Clock Code Below
 
-    time.innerHTML= hour + ":" + minute + ":" + seconds;
+let f = twelvehour;
+f();
+function twelvehour() {
+  var dat = new Date();
+  dat.setHours(14); // for test
+  var h = dat.getHours()
+  var m = dat.getMinutes()
+  var s = dat.getSeconds()
+  if (h >= 12) {
+    time.innerHTML = (h - 12) + ":" + m + ":" + s + "pm"
+  } else {
+    time.innerHTML = h + ":" + m + ":" + s
+  }
 }
 
+function tfourhour() {
+  var dat1 = new Date();
+  dat1.setHours(14); // for test
+  var h1 = dat1.getHours()
+  var m1 = dat1.getMinutes()
+  var s1 = dat1.getSeconds()
+  time.innerHTML = h1 + ":" + m1 + ":" + s1
+}
+setInterval(() => f(), 1000);
+document.getElementById("ampm").onclick = () => { f = twelvehour; f(); }
+document.getElementById("military").onclick = () => { f = tfourhour; f(); }
     
-
-// 12 hour clock code
-
-document.getElementById("ampm").addEventListener("click", function(){
-    const ap = document.getElementById("ampm");
-
-    if(ap.innerHTML == "Switch To 12 Hour Clock"){
-      
-        setInterval(anotherTime,1000)
-
-       function anotherTime(){
-
-        const currentDate = new Date();
-        const hour = currentDate.getHours();
-        const minute = currentDate.getMinutes();
-        const seconds = currentDate.getSeconds();
-        const endingOfClock = "AM"
-
-        if(hour == 0){
-            hour = 12;
-        }
-        if(hour < 10){
-            hour = "0" + hour;
-        }
-       if(hour > 12){
-            hour = hour - 12;
-            endingOfClock = "PM"
-        }
-       if(minute < 10){
-            minute = "0" + minute;
-        }
-        if(seconds < 10){
-            seconds = "0" + minute;
-        } 
-       
-
-       time.innerHTML = hour + ":" + minute + ":" + seconds + endingOfClock;
-       ap.innerHTML= "Switch To Military Time";
-       
-    }
-
-    } else{setInterval(getNewTime,1000);
-
-        function getNewTime(){
-            let currentDate = new Date();
-            let hour = currentDate.getHours();
-            let minute = currentDate.getMinutes();
-            let seconds = currentDate.getSeconds();
-            
-
-
-            time.innerHTML= hour + ":" + minute + ":" + seconds;
-            ap.innerHTML= "Switch to 12 Hour Clock"
-        }   
-    }
-
-});
-
-
 
 // dark mode button code
 
